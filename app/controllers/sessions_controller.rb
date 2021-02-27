37655c4de77b38e3b0052cc8e7c5562935d67a47
高橋和也 <yazuka.000@gmail.com>
2021-02-27 06:48:40 +0000

    advanced-login is making

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
