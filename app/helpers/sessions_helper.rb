52983430baff6349320793e4402480da77d4a0aa
高橋和也 <yazuka.000@gmail.com>
2021-03-01 05:37:55 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,7 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
+      #raise       # テストがパスすれば、この部分がテストされていないことがわかる
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -25,12 +26,14 @@ module SessionsHelper
     end
   end
   
+=begin
   # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+=end
   
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
