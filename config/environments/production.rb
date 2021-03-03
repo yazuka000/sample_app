62a4c2a014b394b134447e90e786438278b22bb6
高橋和也 <yazuka.000@gmail.com>
2021-03-03 06:48:24 +0000

    modify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'https://frozen-springs-75655.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
