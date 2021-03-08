1f25b1564b4e0290138736e75641161e37edf0c5
高橋和也 <yazuka.000@gmail.com>
2021-03-08 05:15:49 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   
   # get '/helf',    to: 'static_pages#help', as: 'helf'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
